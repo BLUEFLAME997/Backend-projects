6ae6e645fdeab57dc2cc511c919e298cfff00461
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-03-13 23:49:39 +0530

    Post creation API created successfully

--- Insta_clone_basic/src/controllers/post.controller.js
+++ Insta_clone_basic/src/controllers/post.controller.js
@@ -1,9 +1,43 @@
+require('dotenv').config();
 const postModel=require('../models/post.model');
+const ImageKit=require('@imagekit/nodejs')
+const {toFile}=require('@imagekit/nodejs')
+const jwt=require('jsonwebtoken');
+
+const imageKit=new ImageKit({
+  privateKey:process.env.IMAGEKIT_PRIVATE_KEY
+})
 
 async function createPostController(req,res){
-  console.log(req.body);
-  console.log(req.file);
-  res.send("post data fetched")
+  const {caption}=req.body;
+  const token=req.cookies.JWT_TOKEN;
+  if(!token){
+    return res.status(401).json({
+      Message:"Token not provided, unauthorized access"
+    })
+  }
+
+  const decoded=jwt.verify(token,process.env.JWT_SECRET)
+  if(!decoded){
+    return res.status(401).json({
+      Message:"Unauthorized access"
+    })
+  }
+  console.log(decoded);
+  const file=await imageKit.files.upload({
+    file:await toFile(Buffer.from(req.file.buffer),'file'),
+    fileName:'Test'
+  })
+
+  const post = await postModel.create({
+    caption:caption,
+    imgUrl:file.url,
+    user:decoded.id
+  })
+  res.status(200).json({
+    message:"ok",
+    file
+  })
 }
 
 module.exports={
