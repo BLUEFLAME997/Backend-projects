6523c8a959d547aebeed4a6922ea302dacc364a5
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-04-24 22:49:22 +0530

    Login page styling

--- Online_course_registration/Frontend/src/pages/Auth/Login.jsx
+++ Online_course_registration/Frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { NavLink } from 'react-router'
+import '../../style/login.scss'
 
 const Login = () => {
   return (
     <section className="login-page">
-      
+      <div className="login">
+        <form>
+          
+          <div className="header">
+            <h1>Welcome back</h1>
+            <p>Sign in to continue</p>
+          </div>
+          
+          <div className="input-fields">
+            <div className="input-contents">
+              <label htmlFor="username">Enter username:</label>
+              <input type="text" placeholder='username' id='username' name='username' />
+            </div>
+            <div className="input-contents">
+              <label htmlFor="password">Enter password:</label>
+              <input type="password" placeholder='abc123' id='password' name='password' />
+            </div>
+          </div>
+
+          <button>Sign In</button>
+        </form>
+        <p>Don't have an account? <NavLink to='/register'>Sign up</NavLink></p>
+      </div>
     </section>
   )
 }
