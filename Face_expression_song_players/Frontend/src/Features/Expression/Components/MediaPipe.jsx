0fd33ada7e91f2b6d775f72755a75ec888381c11
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-08-05 22:26:27 +0530

    Setup

---
+++ Face_expression_song_players/Frontend/src/Features/Expression/Components/MediaPipe.jsx
@@ -0,0 +1,21 @@
+import { FaceDetector, FilesetResolver } from "@mediapipe/tasks-vision";
+
+let detector = null;
+
+export async function createFaceDetector() {
+  if (detector) return detector;
+
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+  );
+
+  detector = await FaceDetector.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/latest/blaze_face_short_range.tflite",
+    },
+    runningMode: "VIDEO",
+  });
+
+  return detector;
+}
