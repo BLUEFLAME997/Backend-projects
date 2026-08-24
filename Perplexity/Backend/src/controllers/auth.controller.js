9db32287ff408686fcdd913f6be50f89829d8552
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-08-24 21:53:49 +0530

    email verification

--- Perplexity/Backend/src/controllers/auth.controller.js
+++ Perplexity/Backend/src/controllers/auth.controller.js
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import userModel from "../model/user.model.js";
 import { sendEmail } from "../services/mail.service.js";
+import jwt from 'jsonwebtoken';
 
 export async function userRegisterController(req, res) {
   const { username, email, password } = req.body;
@@ -24,21 +28,50 @@ export async function userRegisterController(req, res) {
     password
   })
 
+  const emailVerificationToken = jwt.sign({
+    email: user.email
+  }, process.env.JWT_SECRET);
+
   await sendEmail({
     to: email,
     subject: "Welcome to perplexity",
-    html: `<h1>Welcome ${username}! 👋</h1>
-        <p>Your account has been successfully created.</p>
-        `
+    html: `
+  <p>Hi ${username},</p>
+  <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
+  <p>Please verify your email address by clicking the link below:</p>
+  <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+  <p>If you did not create an account, please ignore this email.</p>
+  <p>Best regards,<br>The Perplexity Team</p>
+`
   })
 
   res.status(201).json({
-    Message:"User registered successfully",
-    success:true,
-    user:{
-      id:user._id,
-      username:user.username,
-      email:user.email
+    Message: "User registered successfully",
+    success: true,
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email
     }
   })
+}
+
+export async function verifyEmail(req,res){
+  const token = req.query;
+
+  const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+  const isUserExist = await userModel.findOne({email:decoded.email});
+  if(!isUserExist){
+    return res.status(400).json({
+      Message:"Invalid token",
+      success:false,
+      err:"User not found"
+    })
+  }
+
+  isUserExist.verified = true;
+  await isUserExist.save();
+
+  res.sendHtml(``)
 }
--- Perplexity/Backend/src/routes/auth.route.js
+++ Perplexity/Backend/src/routes/auth.route.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import { registerValidator } from '../validation/auth.validator.js';
 import { userRegisterController } from '../controllers/auth.controller.js';
+import { verifyEmail } from '../controllers/auth.controller.js';
 
 const authRouter =  express.Router();
 
+/* 
+@route: POST /api/auth/register
+@description : To register the user in the application
+*/
 authRouter.post('/register',registerValidator,userRegisterController);
+/* 
+@route: GET /api/auth/verify-email
+@description: To verify the clients details through token 
+*/
+authRouter.get('/verify-email',verifyEmail);
 
 export default authRouter;
